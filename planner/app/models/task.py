757a150b0f51258d04cb36efa2fa97c916293205
m1981 <m.nakiewicz@gmail.com>
2023-08-05 23:47:14 +0000

    Decoupling in progress in separate dir. (chat attached)

--- planner/app/models/task.py
+++ planner/app/models/task.py
@@ -1,25 +1,9 @@
+from dataclasses import dataclass
+
+@dataclass
 class Task:
-    def __init__(self, content=None, labels=None, duration=None):
-        self.content = content
-        self.labels = labels or []
-        self.duration = duration
-
-    # Getters
-    def get_content(self):
-        return self.content
-
-    def get_labels(self):
-        return self.labels
-
-    def get_duration(self):
-        return self.duration
-
-    # Setters
-    def set_content(self, content):
-        self.content = content
-
-    def set_labels(self, labels):
-        self.labels = labels
-
-    def set_duration(self, duration):
-        self.duration = duration
+    title: str
+    label: str
+    duration: int
+    scheduled_date: datetime.datetime = None  # Whatever appropriate value
+    scheduled_start: datetime.datetime = None  # Whatever appropriate value 
--- planner/tests/test_task.py
+++ planner/tests/test_task.py
@@ -1,28 +1,14 @@
 import pytest
-from app.models.task import Task
+from task import Task
 
 
-class TestTask:
-
-  @pytest.fixture
-  def task(self):
-    """Returns a Task instance with predefined properties."""
-    return Task("Test Task", ["Label1", "Label2"], "30min")
-
-  def test_content(self, task):
-    """Tests content getter and setter."""
-    assert task.get_content() == "Test Task"
-    task.set_content("New Task")
-    assert task.get_content() == "New Task"
-
-  def test_labels(self, task):
-    """Tests labels getter and setter."""
-    assert task.get_labels() == ["Label1", "Label2"]
-    task.set_labels(["Label3"])
-    assert task.get_labels() == ["Label3"]
-
-  def test_duration(self, task):
-    """Tests duration getter and setter."""
-    assert task.get_duration() == "30min"
-    task.set_duration("60min")
-    assert task.get_duration() == "60min"
+def test_initialization():
+  title = "a new task"
+  label = "ForMyself"
+  duration = 30
+  task = Task(title, label, duration)
+  assert task.title == title
+  assert task.label == label
+  assert task.duration == duration
+  assert task.scheduled_date is None
+  assert task.scheduled_start is None
